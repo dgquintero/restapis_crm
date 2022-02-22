1ebc1d98c383c2bca1aff3fb1e7d516e22b73f03
Daniel Quintero <dgquintero02@hotmail.com>
2022-02-22 18:17:01 -0500

    configurando postman

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+const mongoose = require('mongoose')
+
+// conectar mongo
+mongoose.promise = global.Promise
+mongoose.connect('mongodb://localhost/restapis', {
+  useNewUrlParser: true
+})
+
 
 // crear el servidor
 const app = express()
