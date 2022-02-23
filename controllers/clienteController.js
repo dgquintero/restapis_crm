2ad71d24fb4281e7eb9865e1fd2e8eede40cfd5a
Daniel Quintero <dgquintero02@hotmail.com>
2022-02-23 17:16:17 -0500

    add rutas de crear y obtener clientes

--- controllers/clienteController.js
+++ controllers/clienteController.js
@@ -13,3 +13,15 @@ exports.nuevoCliente = async (req, res)=> {
     next()
   }
 }
+
+
+// Obtener clientes de la base de datos
+exports.obtenerClientes = async (req, res)=> {
+  try {
+    const clientes = await Clientes.find({})
+    res.json(clientes)
+  } catch (error) {
+    console.log(error)
+    next()
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,7 @@ const clienteController = require('../controllers/clienteController')
 module.exports = function(){
   // Agrega nuevos clientes via POST
   router.post('/clientes', clienteController.nuevoCliente)
+  // Obtener todos los clientes
+  router.get('/clientes', clienteController.obtenerClientes)
   return router
 }
